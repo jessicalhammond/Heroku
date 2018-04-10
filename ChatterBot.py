2e509c0f8eb7fc642f38665e938a44f3531f2884
krazykieka <jessicaleehammond@gmail.com>
2018-04-09 19:01:38 -0700

    work

--- ChatterBot.py
+++ ChatterBot.py
@@ -6,11 +6,6 @@ import time
 import json
 from config import consumer_key, consumer_secret, access_token, access_token_secret
 
-# Twitter API Keys
-consumer_key = "QiF4F4UX2u1as5eDuFMsnp08l"
-consumer_secret = "nDmm0xwRGGqoKL77v7S13kgJXjwlJOzWKYoERHQjqqt8AuLXwJ"
-access_token = "241196245-7upjqJz2Na5Z1mtr8HzGePXl65TtsH7fJVxud62d"
-access_token_secret = "7MgXE1bjim8W0C64gJrs8yC1nmXtIKmT4mgP3ttVbaWwd"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
---
+++ config.py
@@ -0,0 +1,6 @@
+
+# Twitter API Keys
+consumer_key = "QiF4F4UX2u1as5eDuFMsnp08l"
+consumer_secret = "nDmm0xwRGGqoKL77v7S13kgJXjwlJOzWKYoERHQjqqt8AuLXwJ"
+access_token = "241196245-7upjqJz2Na5Z1mtr8HzGePXl65TtsH7fJVxud62d"
+access_token_secret = "7MgXE1bjim8W0C64gJrs8yC1nmXtIKmT4mgP3ttVbaWwd"
